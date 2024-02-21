2f1f99c7639a488b93b87829e9295de5e467adfa
lmangano1 <lmangano1@myjmc.edu.au>
2024-02-22 09:55:03 +1100

    Camera Movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+    void Start()
+    {
+        //Set the offset of the camera faced on the players position
+        offset = transform.position - player.transform.position;
+    }
+
+ 
+    void Update()
+    {
+        //Make the transform position of the camera follow the players transform position
+        transform.position = player.transform.position + offset;
+    }
+}
