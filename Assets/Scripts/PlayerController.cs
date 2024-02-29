f5cc60f15f2a1655da7c333a8eff0e7782ffd22f
lmangano1 <lmangano1@myjmc.edu.au>
2024-02-29 11:56:54 +1100

    Week 3 U.I

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,10 +10,23 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rb;
     private int PickUpCount;
     private Timer timer;
+    private bool gameOver = false;
+
+    [Header("UI")]
+    public TMP_Text PickUpText;
+    public TMP_Text timerText;
+    public TMP_Text WinTimeText;
+    public GameObject WinPanel;
+    public GameObject InGamePanel;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Turn on our In Game Panel
+        InGamePanel.SetActive(true);
+        //Turn off our Win Panel
+        WinPanel.SetActive(false);
+
         rb = GetComponent<Rigidbody>();
         //Get the number of pick ups in our scene
         PickUpCount = GameObject.FindGameObjectsWithTag("Pick Up").Length;
@@ -23,9 +37,16 @@ public class PlayerController : MonoBehaviour
         timer.StartTimer();
     }
 
-    
+    private void Update()
+    {
+        timerText.text = "Time: " + timer.GetTime().ToString("F2");
+    }
+
     void FixedUpdate()
     {
+        if (gameOver == true)
+            return;
+
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
@@ -48,11 +69,42 @@ public class PlayerController : MonoBehaviour
 
     void CheckPickUps()
     {
-        print("Pick Ups Left: " + PickUpCount);
+        PickUpText.text = "Pick Ups Left: " + PickUpCount;
         if (PickUpCount == 0)
         {
-            timer.StopTimer();
-            print("Yay! You Won. Your time was: " + timer.GetTime());
+            WinGame();
         }
     }
+
+    void WinGame ()
+    {
+        //Set our game over to true
+        gameOver = true;
+        //Turn off our In Game Panel
+        InGamePanel.SetActive(false);
+        //Turn on our Win Panel
+        WinPanel.SetActive(true);
+        PickUpText.color = Color.green;
+        PickUpText.fontSize = 60;
+        //Stop the timer
+        timer.StopTimer();
+        // Display our time to the win time text
+        WinTimeText.text = "Your time was: " + timer.GetTime().ToString("F2");
+
+        //Stop the ball from moving
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
+
+    //Temporary - Remove when doing A2 modules
+    public void RestartGame()
+    {
+    UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
